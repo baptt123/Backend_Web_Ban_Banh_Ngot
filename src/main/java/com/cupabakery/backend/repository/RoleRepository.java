236374d2aab2621bde509540fc5814f806b50029
Tiny Devin <trungvan.me@gmail.com>
2025-05-12 14:04:21 +0700

    update role repository

--- src/main/java/com/cupabakery/backend/repository/RoleRepository.java
+++ src/main/java/com/cupabakery/backend/repository/RoleRepository.java
@@ -2,8 +2,10 @@ package com.cupabakery.backend.repository;
 
 import com.cupabakery.backend.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
     Role findByName(String name);
 }
 
