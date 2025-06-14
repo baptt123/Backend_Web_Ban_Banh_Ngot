3d6d609a3080568bf8b64533db478953acf2cc30
Thanhtan101103 <161003191+baptt123@users.noreply.github.com>
2025-06-14 08:56:11 +0700

    feat: add address, phone, and email fields to order request and order entities

--- src/main/java/com/example/myappbackend/model/Orders.java
+++ src/main/java/com/example/myappbackend/model/Orders.java
@@ -45,4 +45,10 @@ public class Orders {
     // --- Thêm trường này nếu chưa có ---
     @Column(name = "paypal_order_id", length = 50)
     private String paypalOrderId;
+    @Column(name = "address", length = 255)
+    private String address;
+    @Column(name = "phone", length = 20)
+    private String phone;
+    @Column(name = "email", length = 150)
+    private String email;
 }
